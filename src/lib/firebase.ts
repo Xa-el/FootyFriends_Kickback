ec1b7ed1d21f1b450c1dd5e58a04962714073968
AntonCSalvador <antoncsalvador@gmail.com>
2024-04-03 18:23:29 -0400

    fixed up files

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdBWL67tGkQ9sVQ8GEn-V7VuxacXP4ddY",
-  authDomain: "footyfriend-47a09.firebaseapp.com",
-  projectId: "footyfriend-47a09",
-  storageBucket: "footyfriend-47a09.appspot.com",
-  messagingSenderId: "854785181568",
-  appId: "1:854785181568:web:18586b73a9b940654ae365",
-  measurementId: "G-43YHFYZXVM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -22,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export { app, analytics };
+
