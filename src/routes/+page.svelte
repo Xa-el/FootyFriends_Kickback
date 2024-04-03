5fe6ab4f99e185ccaaa04381c61063ad53cc8dc5
AntonCSalvador <antoncsalvador@gmail.com>
2024-04-03 02:11:19 -0400

    major bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to FootyFriends</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Visit <a href="/home">lalalalahome</a> to read the documentation</p>
