8ac784f9fae341e51c0642093aed95fb64974455
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-23 00:15:46 -0400

    Bum Page

---
+++ src/routes/Bum/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { collection, getDocs } from 'firebase/firestore';
+	import { db } from '$lib/firebase';
+
+	onMount(async () => {
+		const querySnapshot = await getDocs(collection(db, 'users'));
+
+		querySnapshot.forEach((doc) => {
+			const userData = doc.data();
+			console.log("User ID:", doc.id);
+			console.log("Bio:", userData.bio);
+			console.log("Display Name:", userData.display_name);
+		});
+	});
+</script>
