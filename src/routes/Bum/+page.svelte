80a9d1efa2e1941ea1c5e41947b99951ec259001
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-23 03:41:57 -0400

    randomizer

--- src/routes/Bum/+page.svelte
+++ src/routes/Bum/+page.svelte
@@ -30,7 +30,7 @@
 	const randomPostId = generateRandomString(26);
 
 
-
+	let randomizer = 99999999999999;
 
 
 
@@ -58,9 +58,13 @@
 	};
 
 	async function createPost() {
-		// Adjusted document reference
+		// Adjusted document referenc
 		city = userCity;
-		const cityPostRef = doc(db, userCity, "feed", "posts", randomPostId); // Adjusted path
+		const cityDocRef = collection(db, userCity, "feed", "posts"); // Reference to the "posts" collection within the user's city document
+		const querySnapshot = await getDocs(cityDocRef);
+		randomizer = randomizer-querySnapshot.size;
+
+		const cityPostRef = doc(db, userCity, "feed", "posts", randomizer+randomPostId); // Adjusted path
 		//console.log("user prof ref: " + userProfileRef);
 		try {
 			// Using setDoc with merge true to create or update
@@ -87,23 +91,23 @@
 				userCity = await fetchUserProfile(userId); // Fetch user profile and get the user's city
 				console.log("User city: " + userCity);
 				if (userCity) { // Check if userCity is available
+					const cityDocRef = collection(db, userCity, "feed", "posts"); // Reference to the "posts" collection within the user's city document
+					const querySnapshot = await getDocs(cityDocRef);
 
-					const cityDocRef = doc(db, userCity, "feed", "posts", "msTGdhz7rQ633GW5Igxp");
-					const cityDocSnap = await getDoc(cityDocRef);
-					console.log("got from gainesville db");
-					const postData = cityDocSnap.data();
-					console.log("Post ID:", cityDocSnap.id);
-					console.log("Title:", postData.title);
-					console.log("Caption:", postData.caption);
-					console.log("Display Name:", postData.display_name);
-					console.log("Likes:", postData.likes);
-					console.log("Time:", postData.time);
-					console.log("User ID:", postData.u_id);
-
-					} else {
-						console.log("No such city document!");
-					}
+					querySnapshot.forEach((doc) => {
+						const postData = doc.data();
+						console.log("Post ID:", doc.id);
+						console.log("Title:", postData.title);
+						console.log("Caption:", postData.caption);
+						console.log("Display Name:", postData.display_name);
+						console.log("Likes:", postData.likes);
+						console.log("Time:", postData.time);
+						console.log("User ID:", postData.u_id);
+					});
 				} else {
+					console.log("No such city document!");
+				}
+			} else {
 				// User is not logged in, redirect or handle accordingly
 				goto('/login');
 				const reloadAfterRedirect = () => {
@@ -114,6 +118,7 @@
 			}
 		});
 	});
+
 </script>
 
 <button class="createButton" on:click={createPost}>Create Post</button>
