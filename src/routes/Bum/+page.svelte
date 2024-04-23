db104f2919d0d60b1ee1628d0aa37c42f464c606
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-23 02:34:10 -0400

    Posts kind of working

--- src/routes/Bum/+page.svelte
+++ src/routes/Bum/+page.svelte
@@ -9,7 +9,7 @@
 	let userId;
 	let userCity = '';
 	let pfpURL = '';
-
+	let postId = 'qweqe';
 	const fetchUserProfile = async (userId) => {
 		const docRef = doc(db, "users", userId);
 		const docSnap = await getDoc(docRef);
@@ -26,36 +26,33 @@
 		}
 	};
 
+
+
+
 	onMount(async () => {
 		session.subscribe(async ($session) => {
 			if ($session.user) {
 				userId = $session.user.uid;
 				userCity = await fetchUserProfile(userId); // Fetch user profile and get the user's city
-
+				console.log("User city: " + userCity);
 				if (userCity) { // Check if userCity is available
-					const cityDocRef = doc(db, userCity); // Assuming the collection name is "cities"
+
+					const cityDocRef = doc(db, userCity, "feed", "posts", "msTGdhz7rQ633GW5Igxp");
 					const cityDocSnap = await getDoc(cityDocRef);
+					console.log("got from gainesville db");
+					const postData = cityDocSnap.data();
+					console.log("Post ID:", cityDocSnap.id);
+					console.log("Title:", postData.title);
+					console.log("Caption:", postData.caption);
+					console.log("Display Name:", postData.display_name);
+					console.log("Likes:", postData.likes);
+					console.log("Time:", postData.time);
+					console.log("User ID:", postData.u_id);
 
-					if (cityDocSnap.exists()) {
-						const cityName = cityDocSnap.data().name;
-						const q = query(collection(db, cityName)); // Assuming "cityName" matches the collection name
-						const querySnapshot = await getDocs(q);
-
-						querySnapshot.forEach((doc) => {
-							const postData = doc.data();
-							console.log("Post ID:", doc.id);
-							console.log("Title:", postData.title);
-							console.log("Caption:", postData.caption);
-							console.log("Display Name:", postData.display_name);
-							console.log("Likes:", postData.likes);
-							console.log("Time:", postData.time);
-							console.log("User ID:", postData.u_id);
-						});
 					} else {
 						console.log("No such city document!");
 					}
-				}
-			} else {
+				} else {
 				// User is not logged in, redirect or handle accordingly
 				goto('/login');
 				const reloadAfterRedirect = () => {
@@ -66,5 +63,9 @@
 			}
 		});
 	});
+
+
+
 </script>
+
 
