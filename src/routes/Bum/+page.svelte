163fe541038775b892b827edca9fc10de4176e4e
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-23 03:02:24 -0400

    This is actual money

--- src/routes/Bum/+page.svelte
+++ src/routes/Bum/+page.svelte
@@ -2,23 +2,54 @@
 	import { onMount } from 'svelte';
 	import { session } from '$lib/session.js';
 	import { goto } from '$app/navigation';
-	import { doc, getDoc, query, collection, getDocs } from 'firebase/firestore';
+	import { doc, getDoc, query, collection, getDocs, setDoc } from 'firebase/firestore';
 	import { db } from '$lib/firebase';
+	function generateRandomString(length: number): string {
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		let result = '';
+
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * characters.length);
+			result += characters.charAt(randomIndex);
+		}
+		return result;
+	}
+
+	function getCurrentTimeInEST(): string {
+		const options: Intl.DateTimeFormatOptions = {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZone: 'America/New_York', // EST timezone
+			hour12: false // 24-hour format
+		};
+
+		return new Date().toLocaleString('en-US', options);
+	}
+
+	const randomPostId = generateRandomString(26);
+
+
+
+
+
 
 	let displayName = '';
 	let userId;
 	let userCity = '';
-	let pfpURL = '';
-	let postId = 'qweqe';
+	let title = '';
+	let likesPost = 0;
+	let city = '';
+	let postCaption = 'From code';
+
+
 	const fetchUserProfile = async (userId) => {
 		const docRef = doc(db, "users", userId);
 		const docSnap = await getDoc(docRef);
 
 		if (docSnap.exists()) {
 			displayName = docSnap.data().display_name;
-			pfpURL = docSnap.data().pfpURL;
 			userCity = docSnap.data().City;
-			console.log("Display Name: " + displayName + " PFP URL: " + pfpURL);
 			return userCity; // Return user city for further use
 		} else {
 			console.log("No such document!");
@@ -26,8 +57,28 @@
 		}
 	};
 
+	async function createPost() {
+		// Adjusted document reference
+		city = userCity;
+		const cityPostRef = doc(db, userCity, "feed", "posts", randomPostId); // Adjusted path
+		//console.log("user prof ref: " + userProfileRef);
+		try {
+			// Using setDoc with merge true to create or update
+			await setDoc(cityPostRef, {
+				display_name: displayName,
+				likes : likesPost,
+				u_id : userId,
+				time : getCurrentTimeInEST(),
+				title : title,
+				caption : postCaption,
+			}, { merge: true });
 
+			console.log("Post created successfully");
+		} catch (error) {
+			console.error("Error updating profile: ", error);
+		}
 
+	}
 
 	onMount(async () => {
 		session.subscribe(async ($session) => {
@@ -63,9 +114,9 @@
 			}
 		});
 	});
-
-
-
 </script>
+
+<button class="createButton" on:click={createPost}>Create Post</button>
+
 
 
