7e5ec6c3530c894fe782c5719c3c535d3e86690a
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-04-23 04:02:02 -0400

    finished register/info

--- src/routes/register/info/+page.svelte
+++ src/routes/register/info/+page.svelte
@@ -11,12 +11,12 @@
   let bio = '';
   let city = '';
   let displayName = '';
-  let userId;
+  let userId: any = ''; // Should change later to a specific type
   let pfpURL = '';
   let userCity = '';
 
   // Fetch user profile information
-  const fetchUserProfile = async (userId) => {
+  const fetchUserProfile = async (userId: any) => {
     // Assuming a specific doc ID or using a known doc ID here. Adjust as needed.
     const docRef = doc(db, "users", userId); // Adjusted path
     const docSnap = await getDoc(docRef);
@@ -116,13 +116,13 @@
     }
   });
 
-  function selectCity(city) {
+  function selectCity(city: string) {
     search.set(city);
     userCity = city;
     showDropdown.set(false);
   }
 
-  function checkHideDropdown(event) {
+  function checkHideDropdown(event: any) {
     // Only hide dropdown if the event related target is not a dropdown item
     if (!event.relatedTarget || !event.relatedTarget.classList.contains('dropdown-item')) {
       showDropdown.set(false);
@@ -134,12 +134,7 @@
   .full-height {
     height: 100vh; /* This ensures the parent div takes the full height */
   }
-  .neonLine{
-    outline: 1px solid #6A863D; /* Sets width, style, and color */
-    border-radius: 10px;
-    width: 650px; /* Sets the width of the main container */
-    height: 450px; /* Sets the height of the main container */
-  }
+
 
   .neonLine-Error{
     outline: 1px solid #6A863D; /* Sets width, style, and color */
@@ -155,8 +150,6 @@
     background: #002615;
     color: white;
     border: none;
-    width: 375px; /* Sets the width of the button */
-    height: 50px; /* Sets the height of the button */
     font-size: 18px; /* Adjust this based on your preference */
     padding: 0 15px; /* Horizontal padding, vertical padding is not necessary due to height setting */
     text-align: left;
@@ -195,10 +188,7 @@
   }
   .createButton{
     border: none;
-    background: #C3F53C;
     border-radius: 5px;
-    width: 230px;
-    height: 50px;
     font-size: 20px;
   }
   .close-button {
@@ -222,94 +212,72 @@
      font-size: 24px;
      font-weight: bold;
   }
-
-
-     .dropdown-content {
-       position: absolute;
-       background-color: #f9f9f9;
-       min-width: 160px;
-       box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-       z-index: 1;
-       display: none; /* Initial state set to none, controlled by Svelte */
-     }
-
-     .dropdown-item {
-       padding: 12px 16px;
-       text-decoration: none;
-       display: block;
-       cursor: pointer;
-     }
-
-     .dropdown-item:hover {
-       background-color: #f1f1f1;
-     }
 </style>
-
-<div class="header border-b-2 border-forest-green flex justify-between items-center w-full h-15">
-     <div class="flex flex-row">
-          <div class="logo flex flex-row" >
-               <img src="/logo.png" alt="KICKBACK Logo" />
-               <h1 class="ml-2 font-bold">KICKBACK</h1>
-          </div>
-     </div>
-</div>
-
-
-<div class="flex items-center justify-center overflow-hidden h-[calc(100vh-60px)]">
-  <div>
-    <div class="p-4 bg-side-green neonLine">
-      <div class="centy">
-        <h1 class="mb-5 text-neon-green text-center font-extrabold text-4xl">Account Details</h1>
-        <form on:submit|preventDefault={updateProfile}>
-          <div class="flex flex-row items-center">
-               <p class="font-bold text-neon-green">Display Name:</p>
-               <input class="m-2 inputButton" bind:value={userSeenName} type="text" placeholder="IGN" />
-          </div>
-          <br>
-          <div class="flex flex-row items-center">
-               <p class="font-bold text-neon-green">Bio:</p>
-               <input class="m-2 inputButton" bind:value={displayBio} placeholder="Bio" />
-          </div>
-          <br>
-          <div class="flex flex-row items-center">
-               <p class="font-bold text-neon-green">PFP Image URL:</p>
-               <input class="m-2 inputButton" bind:value={pfpURL} placeholder="Image URL" />
-          </div>
-          <br>
-          <!-- ADD CITY DROP DOWN RIGHT BELOW THIS, TO DO LATER. BUT ALSO FIGURE OUT FIREBASE STUFF-->
-
-          <div class="flex flex-row items-center">
-            <p class="font-bold text-neon-green">City:</p>
-
-            <input
-            class="m-2 inputButton"
-            type="text"
-            placeholder="Type to search..."
-            bind:value={$search}
-            on:focus={() => showDropdown.set(true)}
-            on:blur={checkHideDropdown}
-          />
-
-          <div class="dropdown-content" style:display={$showDropdown ? 'block' : 'none'}>
-            {#each $filteredCities as city}
-              <div
-                on:click={() => selectCity(city)}
-                class="dropdown-item rounded-md border-2 border-neon-green bg-dark-green"
-                tabindex="0">
-                {city}
-              </div>
-            {/each}
-          </div>
-          </div>
-          <button class="createButton text-3xl text-dark-green font-bold" type="submit" >Done</button>
-        </form>
-        <!-- <p class=" mt-2.5 text-neon-green font-bold">Forgot Account?</p> -->
+<div class="flex h-screen w-screen flex-col">
+  <div class="header border-b-2 border-forest-green flex justify-between items-center w-full h-15">
+      <div class="flex flex-row">
+            <div class="logo flex flex-row" >
+                <img src="/logo.png" alt="KICKBACK Logo" />
+                <h1 class="ml-2 font-bold">KICKBACK</h1>
+            </div>
       </div>
-      <div class="newActions font-bold">
+  </div>
+
+
+  <div class="flex items-center justify-center overflow-hidden grow">
+      <div class=" bg-side-green border border-neon-green rounded-lg pt-5 pb-5 pr-8 pl-8 w-2/6">
+          <h1 class="mb-5 text-neon-green text-center font-extrabold text-4xl">Account Details</h1>
+          <form on:submit|preventDefault={updateProfile}>
+            <div class="flex flex-row items-center w-full justify-between">
+                <p class="font-bold text-neon-green mr-10">Display Name:</p>
+                <input class="inputButton w-3/5" bind:value={userSeenName} type="text" placeholder="IGN" />
+            </div>
+            <br>
+            <div class="flex flex-row items-center w-full justify-between">
+                <p class="font-bold text-neon-green">Bio:</p>
+                <input class="inputButton w-3/5" bind:value={displayBio} placeholder="Bio" />
+            </div>
+            <br>
+            <div class="flex flex-row items-center w-full justify-between">
+                <p class="font-bold text-neon-green">PFP Image URL:</p>
+                <input class="inputButton w-3/5" bind:value={pfpURL} placeholder="Image URL" />
+            </div>
+            <br>
+            <!-- ADD CITY DROP DOWN RIGHT BELOW THIS, TO DO LATER. BUT ALSO FIGURE OUT FIREBASE STUFF-->
+
+            <div class="flex flex-row items-center w-full justify-between">
+              <p class="font-bold text-neon-green">City:</p>
+
+              <input
+              class="inputButton w-3/5 border"
+              type="text"
+              id="city_input"
+              placeholder="Type to search..."
+              bind:value={$search}
+              on:focus={() => showDropdown.set(true)}
+              />
+            </div>
+
+            <div class="relative mb-11 w-full flex flex-col items-end">
+              {#if $search.length > 0 && $filteredCities.length > 0 && $showDropdown}
+                <div class="absolute z-10 w-3/5 border bg-side-green rounded-lg">
+                  {#each $filteredCities as city}
+                    <button class="p-2 text-neon-green bg-forest-green w-full hover:cursor-pointer hover:bg-green-700 text-left" on:click={() => {
+                      selectCity(city);
+                      showDropdown.set(false);
+                    }}>{city}</button>
+                  {/each}
+                </div>
+              {/if}
+            </div>
+            <button class="createButton text-3xl text-dark-green font-bold pt-2 pb-2 pr-12 pl-12 bg-neon-green hover:bg-forest-green" type="submit" >Done</button>
+          </form>
+          <!-- <p class=" mt-2.5 text-neon-green font-bold">Forgot Account?</p> -->
+        <div class="newActions font-bold">
+        </div>
       </div>
-    </div>
-    <div class = "textbox">
-    </div>
+      <div class = "textbox">
+      </div>
   </div>
 </div>
 
