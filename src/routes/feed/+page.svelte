fa57cee14bdc32868c80da345d23383717f4f2cb
AntonCSalvador <antoncsalvador@gmail.com>
2024-04-23 22:49:48 -0400

    fixed write logo

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -24,7 +24,10 @@
     <div class="flex flex-col  justify-end items-center h-24 mb-5">
         <div class="flex flex-row w-4/5 mb-4 justify-between">
             <p class="font-bold text-neon-green">Messages</p>
-            <p>[]</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="fill-neon-green w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+
         </div>
 
         <input class="w-4/5 placeholder:text-sm pl-2 rounded-lg" type="text" placeholder="Search">
