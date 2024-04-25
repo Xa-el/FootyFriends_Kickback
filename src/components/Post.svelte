bb50d0b678996f108df69d068b5631048d05a12c
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-25 18:16:48 -0400

    fixed live profile updating for posts

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,15 +1,36 @@
 <!-- Post.svelte -->
-<script>
-	export let post;
-	console.log("Post received:", post); // Log the received post
+<script lang="ts">
+     import { doc, getDoc } from 'firebase/firestore';
+     import { db } from '$lib/firebase.js';
+     import { onMount } from 'svelte';
+
+     let displayName = '';
+     let profile_url = '';
+     export let post;
+	console.log("Post received:", post);
+
+     const fetchUserProfile = async (userId) => {
+          const docRef = doc(db, "users", userId);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+               displayName = docSnap.data().display_name;
+               profile_url = docSnap.data().pfpURL;
+          } else {
+               console.log("No such document!");
+          }
+     }
+
+     onMount(() => {
+          fetchUserProfile(post.userId);
+     });
 </script>
 
 <div class="post text-white">
      <div class="flex items-center">
-          <img src={post.profile_url} class="radi mr-[10px] w-16 h-16 rounded-full border-neon-green border-4">
+          <img src={profile_url} class="radi mr-[10px] w-16 h-16 rounded-full border-neon-green border-4">
           <div>
           <div class="flex items-baseline">
-               <h1 class="text-3xl mr-1">{post.displayName}</h1>
+               <h1 class="text-3xl mr-1">{displayName}</h1>
                <p style="color: #9F9F9F;">{new Date(post.time).toLocaleString()}</p>
           </div>
           <p>{post.caption}</p>
