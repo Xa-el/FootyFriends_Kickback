e94d63efd1bf192523998f13da5f099ed9c796a2
AntonCSalvador <antoncsalvador@gmail.com>
2024-04-25 14:53:37 -0400

    added a bit of margin between time and name

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -9,7 +9,7 @@
           <img src={post.profile_url} class="radi mr-[10px] w-16 h-16 rounded-full border-neon-green border-4">
           <div>
           <div class="flex items-baseline">
-               <h1 class="text-3xl">{post.displayName}</h1>
+               <h1 class="text-3xl mr-1">{post.displayName}</h1>
                <p style="color: #9F9F9F;">{new Date(post.time).toLocaleString()}</p>
           </div>
           <p>{post.caption}</p>
