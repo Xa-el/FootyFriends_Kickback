d49530a01580621c916a162c3a56007e79ee4d02
Xa-el <xael.j.font@gmail.com>
2024-04-27 19:23:30 -0400

    Added active sidebar button (only for feed)

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -5,11 +5,13 @@
     import { goto } from '$app/navigation';
     import { doc, getDoc } from 'firebase/firestore';
     import { db } from '$lib/firebase';
+    import { page } from '$app/stores'; 
 
     let displayName = '';
     let userId;
     let userCity = '';
     let pfpURL = '';
+    let currentPath = '';
 
    const fetchUserProfile = async (userId) => {
         // Assuming a specific doc ID or using a known doc ID here. Adjust as needed.
@@ -41,6 +43,8 @@
                 setTimeout(reloadAfterRedirect, 1000);
             }
         });
+
+        currentPath = window.location.pathname;
     });
 
 
@@ -112,7 +116,7 @@
             <!-- Buttons -->
             <div class="flex flex-col items-left mt-4 space-y-4">
                 <!-- Feed -->
-                <button class="justify-start hover:ring-2 hover:ring-forest-green text-white font-semibold py-1 pl-2 pr-36 rounded-full inline-flex items-left">
+                <button class="justify-start hover:ring-2 hover:ring-forest-green text-white font-semibold py-1 pl-2 pr-36 rounded-full inline-flex items-left {$page.url.pathname.includes('/home') ? 'ring-2 ring-forest-green' : ''}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-forest-green w-6 h-6 mr-2">
                         <path fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clip-rule="evenodd" />
                       </svg>
