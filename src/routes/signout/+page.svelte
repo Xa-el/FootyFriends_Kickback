56af87820819bbdb75635d45de9e822e73ded982
AntonCSalvador <antoncsalvador@gmail.com>
2024-04-27 18:20:31 -0400

    added signout directory temp

---
+++ src/routes/signout/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { getAuth, signOut } from 'firebase/auth';
+
+  // Initialize Firebase auth
+  const auth = getAuth();
+
+  function handleSignOut() {
+    signOut(auth).then(() => {
+      console.log('Signed Out');
+      // Optionally, redirect the user or update the UI state here
+    }).catch((error) => {
+      console.error('Sign Out Error', error);
+    });
+  }
+</script>
+
+<button on:click="{handleSignOut}">Sign Out</button>
+
