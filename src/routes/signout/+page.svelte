35d9fc0cf08454f16fdcbf5b660996bfe3e91121
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-04-28 03:53:22 -0400

    signout

--- src/routes/signout/+page.svelte
+++ src/routes/signout/+page.svelte
@@ -1,18 +1,29 @@
-<script>
-  import { getAuth, signOut } from 'firebase/auth';
+	<script lang="ts">
+		import { session } from '$lib/session';
+		import { auth } from '$lib/firebase';
+		import { onMount } from 'svelte';
+		import { goto } from '$app/navigation';
 
-  // Initialize Firebase auth
-  const auth = getAuth();
+		async function signOut() {
+		await auth.signOut();
+		session.set({
+		loggedIn: false,
+		user: null
+	});
+		goto('/'); // Redirect to the home page after signing out
+	}
 
-  function handleSignOut() {
-    signOut(auth).then(() => {
-      console.log('Signed Out');
-      // Optionally, redirect the user or update the UI state here
-    }).catch((error) => {
-      console.error('Sign Out Error', error);
-    });
-  }
+		// Optional: Redirect to the home page if the user is not logged in
+		onMount(async () => {
+		session.subscribe(($session) => {
+			if (!$session.user) {
+				goto('/');
+			}
+		});
+	});
 </script>
 
-<button on:click="{handleSignOut}">Sign Out</button>
+<div class="flex items-center justify-center">
+	<button class="createButton text-white" on:click={signOut}>Sign Out</button>
+</div>
 
