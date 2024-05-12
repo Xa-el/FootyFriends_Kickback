52d38dd409c28761b0a0652dc94ce97944e8522e
Xa-el <xael.j.font@gmail.com>
2024-05-11 23:50:49 -0400

    Edit profile city, bio, and layout complete

--- src/routes/profile/edit/+page.svelte
+++ src/routes/profile/edit/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { db } from '$lib/firebase';
-  import { collection, doc, getDoc, getDocs, orderBy, query,} from 'firebase/firestore'; // Note: Using setDoc now
+  import { collection, doc, getDoc, getDocs, setDoc, orderBy, query,} from 'firebase/firestore'; // Note: Using setDoc now
   import { session } from '$lib/session';
   import { goto } from '$app/navigation';
   import Post from '../../../components/Post.svelte';
-  import { writable } from 'svelte/store';
+  import { writable, derived } from 'svelte/store';
 
   import "../../../app.css";
 
   let bio = '';
+  let bioCopy = '';
+  let displayBio = ''
   let displayName = '';
   let userId;
   let pfpURL = '';
@@ -22,6 +24,58 @@
   const posts = writable([]);
   const likes = writable([]);
 
+  let cities = ['Gainesville, FL', 'Miami, FL', 'Orlando, FL', 'Tampa, FL', 'Jacksonville, FL'];
+  let showDropdown = writable(false);
+  let search = writable('');
+  let filteredCities = derived(search, $search => {
+    showDropdown.set($search.length > 0);
+    if ($search.length > 0) {
+      return cities.filter(city => city.toLowerCase().includes($search.toLowerCase()));
+    } else {
+      return [];
+    }
+  });
+
+  function selectCity(city: string) {
+    search.set(city);
+    userCity = city;
+    showDropdown.set(false);
+  }
+
+  async function updateProfile() {
+// Adjusted document reference
+if (displayBio == ''){
+  bio = bioCopy;
+}
+else {
+  bio = displayBio;
+}
+let city = userCity;
+
+console.log("This is the bio:" + bio)
+
+const userProfileRef = doc(db, "users", userId); // Adjusted path
+//console.log("user prof ref: " + userProfileRef);
+try {
+  // Using setDoc with merge true to create or update
+  await setDoc(userProfileRef, {
+    bio : bio,
+    display_name: displayName,
+    pfpURL : pfpURL,
+    City : userCity
+  }, { merge: true });
+
+  console.log("Profile updated successfully");
+  // goto('/');
+  //could below this cause an error
+  displayBio = '';
+
+} catch (error) {
+  console.error("Error updating profile: ", error);
+}
+}
+
+
   // Fetch user profile information
   const fetchUserProfile = async (userId) => {
     // Assuming a specific doc ID or using a known doc ID here. Adjust as needed.
@@ -30,6 +84,7 @@
 
     if (docSnap.exists()) {
       bio = docSnap.data().bio;
+      bioCopy = docSnap.data().bio;
       displayName = docSnap.data().display_name;
       pfpURL = docSnap.data().pfpURL;
       userCity = docSnap.data().City;
@@ -124,11 +179,44 @@
     <img src="{pfpURL}" alt={"PFP"} class="w-48 h-48 object-fit rounded-full mt-8">
     <div class="flex flex-col justify-center ml-10">
       <p class="text-white font-bold text-4xl">{displayName}</p>
-      <p class="text-white font-bold text-2xl"><span class="text-neon-green">City:</span> {userCity}</p>
 
-      <div class="w-full overflow-y-auto">
-        <p class="text-white w-full text-xl">{bio}</p>
+      <form on:submit|preventDefault={updateProfile}>
+      
+      <button on:click={() => goto('/profile/')} class="createButton text-3xl text-dark-green font-bold pt-2 pb-2 pr-12 pl-12 bg-neon-green absolute top-16 right-16 rounded-full hover:ring-2 hover:ring-neon-green" type="submit">Save</button>
+      <div class="flex flex-row items-center w-full justify-between">
+        <p class="text-white font-bold text-2xl"><span class="text-neon-green">City:</span>
+        <input
+        class="inputButton w-3/5 border-2 border-forest-green bg-side-green rounded-full placeholder-white font-bold text-2xl pl-3 outline-none focus:border-forest-green"
+        type="text"
+        id="city_input"
+        autocomplete="off"
+        placeholder= {userCity}
+        bind:value={$search}
+        on:focus={() => showDropdown.set(true)}
+        />
       </div>
+      <div class="relative w-full flex flex-col place-items-center">
+        {#if $search.length > 0 && $filteredCities.length > 0 && $showDropdown}
+          <div class="top-full mt-1 md:mt-0 w-full md:w-3/5 absolute z-10 border border-forest-green bg-side-green rounded-lg" style="margin-left: -9%;">
+            {#each $filteredCities as city}
+              <button class="p-2 text-white font-bold w-full hover:cursor-pointer hover:bg-green-700 text-left" on:click={() => {
+                selectCity(city);
+                showDropdown.set(false);
+              }}>{city}</button>
+            {/each}
+          </div>
+        {/if}
+      </div>
+
+      <div class="w-4/6 pt-2 fixed">
+        <input
+        class="inputButton w-full border-2 border-forest-green bg-side-green rounded-lg placeholder-white text-white text-xl pl-3 pt-2 pb-4 outline-none focus:border-forest-green"
+        type="text"
+        bind:value={displayBio}
+        placeholder= {bio}
+        />
+      </div>
+      </form>
 
     </div>
 
@@ -138,11 +226,33 @@
       <div class=" border-b-2 border-forest-green w-full "></div>
   </div>
 
-  <div class="flex w-full justify-center">
-    <div class="mr-16">
-    <p class="font-bold text-2xl text-neon-green">Favorites</p>
+  <div class="flex flex-col w-full items-center">
+    <div class="mr-16 mt-4">
+      <p class="font-bold text-2xl text-neon-green">Favorites</p>
     </div>
+
+      <div class="flex flex-row space-x-96 items-center mr-14 mt-7">
+				<div class="flex flex-col items-center">
+					<img src="https://b.fssta.com/uploads/application/soccer/competition-logos/EnglishPremierLeague.png" alt="league" class="w-36 h-36 mt-4 object-cover">
+					<p class="text-white text-sm md:text-base lg:text-base xl:text-2xl text-center mt-7">League</p>
+				</div>
+
+				<!-- club -->
+				<div class="flex flex-col items-center">
+					<img src="https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/1200px-Real_Madrid_CF.svg.png" alt="club" class="w-36 h-36 mt-4 object-cover">
+					<p class="text-white text-sm md:text-base lg:text-base xl:text-2xl text-center mt-7">Club</p>
+				</div>
+
+				<!-- GOAT -->
+				<div class="flex flex-col items-center">
+					<img src="https://b.fssta.com/uploads/application/soccer/headshots/66617.png" alt="goat" class="w-36 h-36 mt-4 object-cover">
+					<p class="text-white text-sm md:text-base lg:text-base xl:text-2xl text-center mt-7">GOAT</p>
+				</div>
+			</div>
   </div>
+  
+  
+
 
 </div>
 
